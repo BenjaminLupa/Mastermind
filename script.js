7338ec556c78905c360aeb21ceb9c761c1ff6005
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-02-29 08:47:31 +0100

    feat: added for-Loop trys

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ let colors = ["blue", "yellow", "orange", "green", "violet", "purple"];
 
 let randomCode = [];
 let crackTry = 1;
+
+function init() {
+  randomCode = [];
+  crackTry = 1;
+  main_display.innerHTML = "";
+  div_select_colors.innerHTML = "";
+
+  //Versuche
+  for (let i = 1; i <= trys; i++) {
+    let divTry = document.createElement("div");
+    divTry.setAttribute("id", "try-" + i);
+    divTry.setAttribute("class", "try");
+
+    let divLeft = document.createElement("div");
+    divLeft.setAttribute("class", "left");
+
+    let divRight = document.createElement("div");
+    divRight.setAttribute("class", "right");
+  }
+}
